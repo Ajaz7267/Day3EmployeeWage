86758c49eb236cb73bec6b6b62f5ab9096d65058
Ajaz7267 <ajazansari726@gmail.com>
2023-01-19 10:53:13 +0530

    Welcome EmployeeWage

---
+++ EmployeeWage.java
@@ -0,0 +1,33 @@
+//STEP 1
+//Employee Wage Computation Problem
+//Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+//STEP 2
+//Check Employee is Present or Absent
+//- Use ((RANDOM)) for AttendanceCheck
+//STEP 3
+//Calculate Daily Employee Wage
+//Assume Wage Per Hour is 20
+//Assume FullDay Hour is 8
+//STEP 4
+//Add Part time Employee & wage
+//Assume Part Time Hour is 8
+//STEP 5
+//Solving using Switch Case Statement
+//STEP 6
+//Calculating Wages For A Month
+//Assume 20 Working Day Per Month
+//STEP 7
+//Calculate Wages till a condition of total working hours or days is reached for a month
+// - Assume 100 hours and 20 days.
+package EmployeeWage;
+import java.util.Random;
+public class EmployeeWage {
+    public static void main(String args[]) {
+
+
+        {
+            System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        }
+    }
+}
+
